109a485e6b313f834d79f82c407f11e2c0357629
operator.name <operator.name@protonmail.com>
2020-07-09 23:14:26 +0100

    Use nix-shell as shabang and slightly improve printing

--- pages/popup/ping-bench-serial.sh
+++ pages/popup/ping-bench-serial.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash --pure -p go-pup iputils 
 
-N=10;
+N=1;
 
-printf "average\tmin\tmax\tstd\thostname\n"
+printf "average\tmin\tmax\tstd\thostname\n";
 for i in $(echo -e "nitter-instance-list\ninvidious-instances-list\nbibliogram-instance-list"); do 
 	for url in $(cat popup.html | pup "datalist#$i" | pup 'option attr{value}'); do 
 		host=$(echo $url | sed 's:.*//::'); 
-		ping -c "$N" "$host" | tail -1 | awk '{print $4}' | awk -F '/' -v OFS='\t' -v ORS='\t' '{print $2, $1, $3, $4}' && printf "$host\n"
+		ping -c "$N" "$host" 2> /dev/null | ((tail -1 | awk '{print $4}' | awk -F '/' -v OFS='\t' -v ORS='\t' '{print $2, $1, $3, $4}') && echo "$host");
 	done; 
 	echo; 
 done
